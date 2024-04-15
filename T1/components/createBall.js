289a645ffadce371031a3bbca012c6c0cbee5e51
Luca-Panza <luca.panza.007@gmail.com>
2024-04-15 12:01:16 -0300

    feat: implement tank shooting mechanics with keyboard controls
    - Implement shooting functionality with cooldown management.
    
    - Add Ball class to handle the physics and geometry of projectiles.

--- T1/components/createBall.js
+++ T1/components/createBall.js
@@ -1,50 +1,49 @@
-import * as THREE from 'three';
-import { scene } from '../constants/constants.js';
+import * as THREE from "three";
+import { scene } from "../constants/constants.js";
 //-- Ball Class -----------------------------------------------------------
 export class Ball {
-    constructor() {
-        this.speed = 0.1;
-        this.moveOn = true;
-        this.direction = new THREE.Vector3(0.7, 0.0, 0.4).normalize();
-        this.object = this.buildGeometry()
-        scene.add(this.object);
-    }
-    getSpeed() {
-        return this.speed;
-    }
-    setSpeed(speed) {
-        this.speed = speed;
-    }
-    startMoving(move) {
-        this.moveOn = move;
-    }
-    move() {
-        if (!this.moveOn) return;
-        let step = this.direction.clone().multiplyScalar(this.speed);
-        this.object.position.add(step);
+  constructor(direction) {
+    this.speed = 0.1;
+    this.moveOn = true;
+    this.direction = direction.normalize(); // Utilize a direção passada
+    this.object = this.buildGeometry();
+    scene.add(this.object);
+  }
+  getSpeed() {
+    return this.speed;
+  }
+  setSpeed(speed) {
+    this.speed = speed;
+  }
+  startMoving(move) {
+    this.moveOn = move;
+  }
+  move() {
+    if (!this.moveOn) return;
+    let step = this.direction.clone().multiplyScalar(this.speed);
+    this.object.position.add(step);
 
-        this.checkCollisions();
-    }
-    checkCollisions() {
-        // Aqui pode-se incluir critérios de colisão mais sofisticados. 
-        // Neste exemplo mais simples o controle é feito analisando as fronteiras do plano
-        let size = 4.5;
-        if (this.object.position.x > size) this.changeDirection(new THREE.Vector3(-1.0, 0.0, 0.0));
-        if (this.object.position.x < -size) this.changeDirection(new THREE.Vector3(1.0, 0.0, 0.0));
-        if (this.object.position.z > size) this.changeDirection(new THREE.Vector3(0.0, 0.0, -1.0));
-        if (this.object.position.z < -size) this.changeDirection(new THREE.Vector3(0.0, 0.0, 1.0));
-    }
-    changeDirection(normal) {
-        this.direction.reflect(normal).normalize();
-    }
-    setDirection(direction) {
-        this.direction = direction.normalize();
-    }
-    buildGeometry() {
-        let obj = new THREE.Mesh(new THREE.SphereGeometry(0.5, 32, 32),
-            new THREE.MeshPhongMaterial({ color: "red", shininess: "200" }));
-        obj.position.set(0, 0.5, 0);
-        obj.castShadow = true;
-        return obj;
-    }
+    this.checkCollisions();
+  }
+  checkCollisions() {
+    // Aqui pode-se incluir critérios de colisão mais sofisticados.
+    // Neste exemplo mais simples o controle é feito analisando as fronteiras do plano
+    let size = 4.5;
+    if (this.object.position.x > size) this.changeDirection(new THREE.Vector3(-1.0, 0.0, 0.0));
+    if (this.object.position.x < -size) this.changeDirection(new THREE.Vector3(1.0, 0.0, 0.0));
+    if (this.object.position.z > size) this.changeDirection(new THREE.Vector3(0.0, 0.0, -1.0));
+    if (this.object.position.z < -size) this.changeDirection(new THREE.Vector3(0.0, 0.0, 1.0));
+  }
+  changeDirection(normal) {
+    this.direction.reflect(normal).normalize();
+  }
+  setDirection(direction) {
+    this.direction = direction.normalize();
+  }
+  buildGeometry() {
+    let obj = new THREE.Mesh(new THREE.SphereGeometry(0.5, 32, 32), new THREE.MeshPhongMaterial({ color: "white", shininess: "200" }));
+    obj.position.set(0, 0.5, 0);
+    obj.castShadow = true;
+    return obj;
+  }
 }
--- T1/controls/keyBoardControl.js
+++ T1/controls/keyBoardControl.js
@@ -1,3 +1,5 @@
+import * as THREE from "three";
+
 import KeyboardState from "../../libs/util/KeyboardState.js";
 import { moveDistance, rotateAngle } from "../constants/constants.js";
 import { Ball } from "../components/createBall.js";
@@ -5,13 +7,20 @@ import { Ball } from "../components/createBall.js";
 // Instância do KeyboardState
 var keyboard = new KeyboardState();
 
+// Gerenciador de bolas
+var ballsTank1 = [];
+var ballsTank2 = [];
+
+// Controles de temporização para atirar
+var lastShotTimeTank1 = 0;
+var lastShotTimeTank2 = 0;
+const shotCoolDown = 2000; // Tempo de CoolDown em milissegundos (2000 ms = 2 segundos)
+
 function keyboardUpdateTank1(tank1, bbTank1) {
   keyboard.update();
 
-  // salvando a posição anterior do tanque para restaurar em caso de colisão
   tank1.previousPosition = tank1.position.clone();
 
-  // movimentação do tanque
   if (keyboard.pressed("W")) tank1.translateX(moveDistance);
   if (keyboard.pressed("S")) tank1.translateX(-moveDistance);
   if (keyboard.pressed("A")) tank1.rotateY(rotateAngle);
@@ -19,11 +28,16 @@ function keyboardUpdateTank1(tank1, bbTank1) {
 
   bbTank1.setFromObject(tank1);
 
-  let ball = new Ball();
-  if (keyboard.pressed("space")) {
+  if (keyboard.pressed("space") && Date.now() - lastShotTimeTank1 >= shotCoolDown) {
+    lastShotTimeTank1 = Date.now();
+    let direction = new THREE.Vector3(0, 0, 1); // Direção frente em relação ao tanque
+    direction.applyQuaternion(tank1.quaternion); // Aplica a rotação do tanque ao vetor
+    let ball = new Ball(direction);
+    ball.object.position.copy(tank1.position);
     ball.startMoving(true);
+    ballsTank1.push(ball);
   }
-  ball.move();
+  ballsTank1.forEach((ball) => ball.move());
 }
 
 function keyboardUpdateTank2(tank2, bbTank2) {
@@ -38,8 +52,48 @@ function keyboardUpdateTank2(tank2, bbTank2) {
   if (keyboard.pressed("left")) tank2.rotateY(rotateAngle);
   if (keyboard.pressed("right")) tank2.rotateY(-rotateAngle);
 
-  // toda vez que anda com o tanque também tem que fazer com que o bound box ande com ele
   bbTank2.setFromObject(tank2);
+
+  // Verifica o tempo para permitir atirar novamente
+  if (keyboard.pressed(",") && Date.now() - lastShotTimeTank2 >= shotCoolDown) {
+    lastShotTimeTank2 = Date.now();
+    let direction = new THREE.Vector3(0, 0, 1); // Direção frente em relação ao tanque
+    direction.applyQuaternion(tank2.quaternion); // Aplica a rotação do tanque ao vetor
+    let ball = new Ball(direction);
+    ball.object.position.copy(tank2.position);
+    ball.startMoving(true);
+    ballsTank2.push(ball);
+  }
+  ballsTank2.forEach((ball) => ball.move());
 }
+
+/*
+
+function keyboardUpdateTank2(tank2, bbTank2) {
+  keyboard.update();
+
+  // salvando a posição anterior do tanque para restaurar em caso de colisão
+  tank2.previousPosition = tank2.position.clone();
+
+  // movimentação do tanque
+  if (keyboard.pressed("up")) tank2.translateX(moveDistance);
+  if (keyboard.pressed("down")) tank2.translateX(-moveDistance);
+  if (keyboard.pressed("left")) tank2.rotateY(rotateAngle);
+  if (keyboard.pressed("right")) tank2.rotateY(-rotateAngle);
+
+  bbTank2.setFromObject(tank2);
+
+  // Verifica o tempo para permitir atirar novamente
+  if (keyboard.pressed(",") && Date.now() - lastShotTimeTank2 >= shotCoolDown) {
+    lastShotTimeTank2 = Date.now();
+    let ball = new Ball();
+    ball.object.position.copy(tank2.position); // Ajuste a posição do objeto Mesh dentro de Ball
+    ball.startMoving(true);
+    ballsTank2.push(ball);
+  }
+  ballsTank2.forEach((ball) => ball.move());
+}
+
+*/
 
 export { keyboardUpdateTank1, keyboardUpdateTank2 };
