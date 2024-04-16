33cc7ccb14032b1b49ec8e7f509c758f739da00c
Luca-Panza <luca.panza.007@gmail.com>
2024-04-16 09:33:03 -0300

    feat: add cooldown and alternative firing keys for tanks

--- T1/controls/keyBoardControl.js
+++ T1/controls/keyBoardControl.js
@@ -11,6 +11,11 @@ var keyboard = new KeyboardState();
 var ballsTank1 = [];
 var ballsTank2 = [];
 
+// Controles de temporização para atirar
+var lastShotTimeTank1 = 0;
+var lastShotTimeTank2 = 0;
+const shotCoolDown = 500;
+
 function keyboardUpdateTank1(tank1, bbTank1) {
   keyboard.update();
 
@@ -23,7 +28,8 @@ function keyboardUpdateTank1(tank1, bbTank1) {
 
   bbTank1.setFromObject(tank1);
 
-  if (keyboard.down("space")) {
+  if ((keyboard.pressed("space") || keyboard.pressed("Q")) && Date.now() - lastShotTimeTank1 >= shotCoolDown) {
+    lastShotTimeTank1 = Date.now();
     let direction = new THREE.Vector3(0, 0, 1); // Direção frente em relação ao tanque
     direction.applyQuaternion(tank1.quaternion); // Aplica a rotação do tanque ao vetor
     let ball = new Ball(direction);
@@ -49,7 +55,8 @@ function keyboardUpdateTank2(tank2, bbTank2) {
   bbTank2.setFromObject(tank2);
 
   // Verifica o tempo para permitir atirar novamente
-  if (keyboard.down(",") ) {
+  if ((keyboard.pressed(",") || keyboard.pressed("/")) && Date.now() - lastShotTimeTank2 >= shotCoolDown) {
+    lastShotTimeTank2 = Date.now();
     let direction = new THREE.Vector3(0, 0, 1); // Direção frente em relação ao tanque
     direction.applyQuaternion(tank2.quaternion); // Aplica a rotação do tanque ao vetor
     let ball = new Ball(direction);
@@ -57,36 +64,6 @@ function keyboardUpdateTank2(tank2, bbTank2) {
     ball.startMoving(true);
     ballsTank2.push(ball);
   }
-  ballsTank2.forEach((ball) => ball.move());
 }
-
-/*
-
-function keyboardUpdateTank2(tank2, bbTank2) {
-  keyboard.update();
-
-  // salvando a posição anterior do tanque para restaurar em caso de colisão
-  tank2.previousPosition = tank2.position.clone();
-
-  // movimentação do tanque
-  if (keyboard.pressed("up")) tank2.translateX(moveDistance);
-  if (keyboard.pressed("down")) tank2.translateX(-moveDistance);
-  if (keyboard.pressed("left")) tank2.rotateY(rotateAngle);
-  if (keyboard.pressed("right")) tank2.rotateY(-rotateAngle);
-
-  bbTank2.setFromObject(tank2);
-
-  // Verifica o tempo para permitir atirar novamente
-  if (keyboard.pressed(",") && Date.now() - lastShotTimeTank2 >= shotCoolDown) {
-    lastShotTimeTank2 = Date.now();
-    let ball = new Ball();
-    ball.object.position.copy(tank2.position); // Ajuste a posição do objeto Mesh dentro de Ball
-    ball.startMoving(true);
-    ballsTank2.push(ball);
-  }
-  ballsTank2.forEach((ball) => ball.move());
-}
-
-*/
 
 export { keyboardUpdateTank1, keyboardUpdateTank2 };
