75834167ab653bf6ef0b54a8a49012fd122398aa
Luca-Panza <luca.panza.007@gmail.com>
2024-04-17 07:54:48 -0300

    feat: Add enhanced visuals to level creation

--- T1/components/createLevel.js
+++ T1/components/createLevel.js
@@ -5,6 +5,7 @@ import { bbWalls, walls } from "../constants/constants.js";
 function createLevel(levelData, planeWidth, planeHeight, scene) {
   const wallGeometry = new THREE.BoxGeometry(5, 5, 5);
   const wallMaterial = new THREE.MeshBasicMaterial({ color: "grey" });
+  const edgeMaterial = new THREE.LineBasicMaterial({ color: "white", linewidth: 3 });
 
   const blockSize = 5;
   const offsetX = -(planeWidth / 2 - blockSize / 2);
@@ -18,11 +19,16 @@ function createLevel(levelData, planeWidth, planeHeight, scene) {
         const posY = 2.5;
         const posZ = -i * blockSize + offsetZ;
         wall.position.set(posX, posY, posZ);
-        let bbWall = new THREE.Box3().setFromObject(wall);
 
+        // Adicionando arestas
+        const edges = new THREE.EdgesGeometry(wallGeometry);
+        const line = new THREE.LineSegments(edges, edgeMaterial);
+        line.position.set(posX, posY, posZ);
+        scene.add(line);
+
+        let bbWall = new THREE.Box3().setFromObject(wall);
         // Definir normal padrão para as paredes do centro
         let normal = new THREE.Vector3(1, 0, 0); // Escolha uma normal padrão para paredes centrais
-
         // Verificar e alterar a normal para paredes de borda
         if (i === 0) {
           normal = new THREE.Vector3(0, 0, 1);
@@ -33,7 +39,6 @@ function createLevel(levelData, planeWidth, planeHeight, scene) {
         } else if (j === levelData[i].length - 1) {
           normal = new THREE.Vector3(-1, 0, 0);
         }
-
         // Armazenar a normal calculada com a bbWall
         bbWall.normal = normal;
 
@@ -46,34 +51,3 @@ function createLevel(levelData, planeWidth, planeHeight, scene) {
 }
 
 export { createLevel };
-
-/*
-import * as THREE from "three"; 
-import { walls , bbWalls } from "../constants/constants.js"
- 
-function createLevel(levelData, planeWidth, planeHeight, scene) {
-  const wallGeometry = new THREE.BoxGeometry(5, 5, 5);
-  const wallMaterial = new THREE.MeshBasicMaterial({ color: "grey" });
-
-  const blockSize = 5;
-  const offsetX = -(planeWidth / 2 - blockSize / 2);
-  const offsetZ = planeHeight / 2 - blockSize / 2;
-
-  for (let i = 0; i < levelData.length; i++) {
-    for (let j = 0; j < levelData[i].length; j++) {
-      if (levelData[i][j] === 1) {
-        const wall = new THREE.Mesh(wallGeometry, wallMaterial);
-        const posX = j * 5 + offsetX;
-        const posY = 2.5;
-        const posZ = -i * 5 + offsetZ;
-        wall.position.set(posX, posY, posZ);
-        let bbWall = new THREE.Box3().setFromObject(wall);
-        walls.push(wall);
-        bbWalls.push(bbWall);
-        scene.add(wall);
-      }
-    }
-  }
-}
-
-export { createLevel }; */
