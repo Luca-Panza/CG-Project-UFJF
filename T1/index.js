642a0b75ba3d76acac59e7d4323156131ebf4fd0
Eduarda Araujo <74570744+eduardaac@users.noreply.github.com>
2024-08-01 22:11:28 -0300

    Responsividade: OK

--- T1/index.js
+++ T1/index.js
@@ -9,7 +9,6 @@ import {
   initCamera,
   initDefaultBasicLight,
   setDefaultMaterial,
-  onWindowResize,
   createGroundPlaneXZ,
 } from "../libs/util/util.js";
 import { SecondaryBoxTopEsquerda, SecondaryBoxTopDireita } from "./util/util.js";
@@ -23,51 +22,92 @@ import { updateCameraPosition } from "./controls/cameraControl.js";
 import { createBBHelper } from "./helpers/bbHelper.js";
 import { levels, scene, bbWalls } from "./constants/constants.js";
 
-let renderer, camera, material, light, orbit, prevCameraPosition; // Variável global para a posição anterior da câmera
-let orbitControlsEnabled = false; // Variável global para controlar se os controles orbitais estão ativados
-let currentLevelIndex = 0; // Index do nível atual
+let renderer, camera, material, light, orbit, prevCameraPosition;
+let orbitControlsEnabled = false;
+let currentLevelIndex = 0;
 
-renderer = initRenderer(); // Inicialização do renderizador
-camera = initCamera(new THREE.Vector3(0, 15, 30)); // Inicialização da câmera
-material = setDefaultMaterial(); // Criação de um material padrão
-light = initDefaultBasicLight(scene); // Criação de uma luz padrão
-orbit = new OrbitControls(camera, renderer.domElement); // Inicialização dos controles orbitais (inicialmente desativados)
-orbit.enabled = false; // Desativar os controles orbitais
+const initialWidth = 85;
+const initialHeight = 60;
 
-// Adicionando um listener para o evento de resize
-window.addEventListener(
-  "resize",
-  function () {
-    onWindowResize(camera, renderer);
-  },
-  false
-);
+let planeWidth = initialWidth;
+let planeHeight = initialHeight;
 
-// Adicionando um listener para a tecla 'o' para habilitar/desabilitar os controles orbitais
-window.addEventListener("keydown", function (event) {
-  if (event.key === "o") {
-    orbitControlsEnabled = !orbitControlsEnabled;
+function init() {
+    renderer = initRenderer();
+    camera = initCamera(new THREE.Vector3(0, 15, 30));
+    material = setDefaultMaterial();
+    light = initDefaultBasicLight(scene);
+    orbit = new OrbitControls(camera, renderer.domElement);
+    orbit.enabled = false;
 
-    if (orbitControlsEnabled) {
-      // Se os controles orbitais estiverem sendo ativados, salve a posição atual da câmera
-      prevCameraPosition = camera.position.clone();
-      orbit.enabled = true;
-    } else {
-      // Se os controles orbitais estiverem sendo desativados, restaure a posição da câmera
-      orbit.enabled = false;
-      camera.position.copy(prevCameraPosition);
+    // Configuração inicial do canvas
+    updateRendererSize();
+    updateCameraAspect();
+    updateGroundPlane();
+}
+
+function updateRendererSize() {
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.setPixelRatio(window.devicePixelRatio);
+}
+
+function updateCameraAspect() {
+    const aspect = Math.max(window.innerWidth / window.innerHeight);
+    camera.aspect = aspect;
+    camera.updateProjectionMatrix();
+}
+
+function updateGroundPlane() {
+    // Remove o plano antigo
+    const oldPlane = scene.getObjectByName('groundPlane');
+    if (oldPlane) {
+        scene.remove(oldPlane);
     }
-  }
+
+    // Ajusta o tamanho do plano com base na nova proporção
+    planeWidth = Math.max(initialWidth * (window.innerWidth / window.innerHeight), initialWidth);
+    planeHeight = Math.max(initialHeight, initialHeight * (window.innerHeight / window.innerWidth));
+
+    const plane = createGroundPlaneXZ(planeWidth, planeHeight);
+    plane.name = 'groundPlane';
+    scene.add(plane);
+}
+
+function onWindowResize() {
+    updateRendererSize();
+    updateCameraAspect();
+    updateGroundPlane();
+}
+
+init();
+
+window.addEventListener("resize", onWindowResize, false);
+
+window.addEventListener("keydown", function (event) {
+    if (event.key === "o") {
+        orbitControlsEnabled = !orbitControlsEnabled;
+
+        if (orbitControlsEnabled) {
+            prevCameraPosition = camera.position.clone();
+            orbit.enabled = true;
+        } else {
+            orbit.enabled = false;
+            camera.position.copy(prevCameraPosition);
+        }
+    }
+    else if(event.key === "1"){
+        currentLevelIndex = 0;
+        resetaJogo();
+    }
+    else if(event.key == "2"){
+        currentLevelIndex = 1;
+        resetaJogo();
+
+    }
 });
 
-// Criando o plano
-const planeWidth = 85;
-const planeHeight = 60;
-let plane = createGroundPlaneXZ(planeWidth, planeHeight);
-scene.add(plane);
-
-// Função para criar o nível
-createLevel(levels[currentLevelIndex], planeWidth, planeHeight, scene);
+// Criando o nível com as dimensões iniciais
+createLevel(levels[currentLevelIndex], planeWidth/2, planeHeight, scene);
 
 // Criando os tanques
 let tank1 = new Tank(0xff0000, new THREE.Vector3(-20, 0, 15));
@@ -81,11 +121,9 @@ scene.add(tank2.object);
 let bbTank1 = new THREE.Box3();
 let bbTank2 = new THREE.Box3();
 
-// Definindo os bounding boxes dos tanques
 bbTank1.setFromObject(tank1.object);
 bbTank2.setFromObject(tank2.object);
 
-// Adicionando os bounding boxes dos tanques à cena
 let bbHelper1 = createBBHelper(bbTank1, "white");
 let bbHelper2 = createBBHelper(bbTank2, "white");
 scene.add(bbHelper1);
@@ -98,44 +136,41 @@ buildTutorial();
 const placar1 = new SecondaryBoxTopEsquerda();
 const placar2 = new SecondaryBoxTopDireita();
 
-// Função para mostrar o placar
 function mostraPlacar() {
-  placar1.changeMessage("Dano Tanque 1 (Vermelho) " + tank1.dano);
-  placar2.changeMessage("Dano Tanque 2 (Azul) " + tank2.dano);
+    placar1.changeMessage("Dano Tanque 1 (Vermelho) " + tank1.dano);
+    placar2.changeMessage("Dano Tanque 2 (Azul) " + tank2.dano);
 }
 
-// Função para reiniciar o jogo
 function resetaJogo() {
-  tank1.dano = 0;
-  tank2.dano = 0;
-  location.reload();
+    tank1.dano = 0;
+    tank2.dano = 0;
+    location.reload();
 }
 
 function VerificaPlacar() {
-  if (tank1.dano >= 10) {
-    alert("Tanque 2 (azul) venceu!");
-    resetaJogo();
-  } else if (tank2.dano >= 10) {
-    alert("Tanque 1 (vermelho) venceu!");
-    resetaJogo();
-  }
+    if (tank1.dano >= 10) {
+        alert("Tanque 2 (azul) venceu!");
+        resetaJogo();
+    } else if (tank2.dano >= 10) {
+        alert("Tanque 1 (vermelho) venceu!");
+        resetaJogo();
+    }
 }
 
 function render() {
-  keyboardUpdateTank1(tank1, bbTank1, tank2, bbTank2);
-  keyboardUpdateTank2(tank2, bbTank2, tank1, bbTank1);
-  checkCollisions(tank1.object, bbTank1, tank2.object, bbTank2, bbWalls);
-  updateCameraPosition(camera, tank1.object, tank2.object, orbitControlsEnabled);
+    keyboardUpdateTank1(tank1, bbTank1, tank2, bbTank2);
+    keyboardUpdateTank2(tank2, bbTank2, tank1, bbTank1);
+    checkCollisions(tank1.object, bbTank1, tank2.object, bbTank2, bbWalls);
+    updateCameraPosition(camera, tank1.object, tank2.object, orbitControlsEnabled);
 
-  mostraPlacar();
-  VerificaPlacar();
+    mostraPlacar();
+    VerificaPlacar();
 
-  // Ajustando a visibilidade dos helpers
-  bbHelper1.visible = false;
-  bbHelper2.visible = false;
+    bbHelper1.visible = false;
+    bbHelper2.visible = false;
 
-  requestAnimationFrame(render);
-  renderer.render(scene, camera);
+    requestAnimationFrame(render);
+    renderer.render(scene, camera);
 }
 
 render();
