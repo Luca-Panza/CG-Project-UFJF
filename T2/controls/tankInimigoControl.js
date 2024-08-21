9eb548be74804b677cfd4c6b1eb1acac86af1250
MariaClar11 <mcrm74161@gmail.com>
2024-08-21 17:23:22 -0300

    tiro do tanque inimigo nivel 1

--- T2/controls/tankInimigoControl.js
+++ T2/controls/tankInimigoControl.js
@@ -1,12 +1,55 @@
 import * as THREE from "three";
+import { Ball } from "../components/createBall.js";
 
+// Gerenciador de bolas
+const ballsTank = [];
+
+// Variável para armazenar o tempo do último disparo
+let lastShootTime = 0;
+const shootInterval = 2000; // Intervalo de tempo em milissegundos (2 segundos)
+
+// Função de disparo
+function shoot(
+  tank,
+  index,
+  tankUsuario,
+  bbTankUsuario,
+  tankInimigo2,
+  bbtankInimigo2
+) {
+  // Lógica do Tiro
+  const direction = new THREE.Vector3();
+  tank.object.getWorldDirection(direction);
+
+  // Ajuste para a direção do tiro baseado na rotação do tanque
+  const axisY = new THREE.Vector3(0, 0, 0);
+  direction.applyAxisAngle(axisY, Math.PI / 2);
+
+  // Escolher o inimigo com base no valor de index
+  let targetTank, targetBoundingBox;
+  if (index === 0) {
+    targetTank = tankUsuario;
+    targetBoundingBox = bbTankUsuario;
+  } else if (index === 1) {
+    targetTank = [tankUsuario, tankInimigo2];
+    targetBoundingBox = [bbTankUsuario, bbtankInimigo2];
+  }
+
+  const ball = new Ball(direction, targetTank, targetBoundingBox, index);
+  ball.object.position.set(tank.object.position.x, 3, tank.object.position.z);
+  ball.startMoving(true);
+  ballsTank.push(ball);
+}
+
+// Função de comportamento do tanque inimigo
 function enemyTankBehavior(
   index,
   tank,
   bbTank,
   tankUsuario,
   bbTankUsuario,
-  bbWalls
+  tankInimigo2 = null,
+  bbtankInimigo2 = null
 ) {
   const speed = 0.1;
   const backwardSpeed = speed; // Velocidade de movimentação para trás
@@ -20,7 +63,7 @@ function enemyTankBehavior(
     tank.object.translateZ(speed);
   } else if (distance == safeDistance) {
     // Se a distância for exatamente a distância segura, apenas olhar para o usuário
-    tank.object.lookAt(tankUsuario.object.position);
+    // tank.object.lookAt(tankUsuario.object.position);
   } else {
     // Se a distância for menor que a distância segura, fugir do usuário
     const direction = new THREE.Vector3();
@@ -37,20 +80,22 @@ function enemyTankBehavior(
   // Atualizar o bounding box após mover o tanque
   bbTank.setFromObject(tank.object);
 
-  // comentando essa lógica porque não funcionou e ficou com muitos bugs da colisão com a parede
-  // Verificar colisão com as paredes
-  // for (let i = 0; i < bbWalls.length; i++) {
-  //   const bbWall = bbWalls[i]; // Bounding box da parede atual
-  //   if (bbWall.intersectsBox(bbTank)) {
-  //     tank.object.rotateY(-(Math.PI / 4)); // Rotaciona para evitar a parede
-  //     bbTank.setFromObject(tank.object);
-  //   }
-  //   while (bbWall.intersectsBox(bbTank)) {
-  //     tank.object.previousPosition = tank.object.position.clone();
-  //     tank.object.translateZ(speed / 4); // Move devagar para corrigir a colisão
-  //     bbTank.setFromObject(tank.object);
-  //   }
-  // }
+  // Verificar se o tempo definido se passou desde o último disparo
+  const currentTime = performance.now();
+  if (currentTime - lastShootTime >= shootInterval) {
+    shoot(
+      tank,
+      index,
+      tankUsuario,
+      bbTankUsuario,
+      tankInimigo2,
+      bbtankInimigo2
+    );
+    lastShootTime = currentTime; // Atualizar o tempo do último disparo
+  }
+
+  // Mover as bolas disparadas
+  ballsTank.forEach((ball) => ball.move());
 }
 
 export { enemyTankBehavior };
